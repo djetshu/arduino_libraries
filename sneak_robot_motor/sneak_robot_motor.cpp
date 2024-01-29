502697c4a9cdab22059cce845742d518904e8004
jetshu <queque.daffer@pucp.edu.pe>
2024-01-29 12:42:19 -0500

    Adding PID, Flex sensor and snake_robot_motor libraries

---
+++ sneak_robot_motor/sneak_robot_motor.cpp
@@ -0,0 +1,98 @@
+#if ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#include <sneak_robot_motor.h>
+#include <PID_v1.h>
+
+/*Constructor (...)*********************************************************
+ *    The parameters specified here are those for for which we can't set up
+ *    reliable defaults, so we need to have the user set them.
+ ***************************************************************************/
+Motor_Sneak::Motor_Sneak(int pinPWM, int pinINA, int pinINB,  
+                          const float PPR_motor, const float gearRatio_motor, const float decodeNumber_motor, 
+                          const int encoder_PinA, const int encoder_PinB, 
+                          double* InputPID, double* OutputPID, double* SetpointPID, 
+                          double KpPID, double KiPID, double KdPID, float limitOutputPID, float sampleTimePID)
+    : pid(InputPID, OutputPID, SetpointPID, KpPID, KiPID, KdPID, DIRECT),  // Initialize PID object here
+      PWM(pinPWM), INA(pinINA), INB(pinINB),
+      PPR(PPR_motor), gearRatio(gearRatio_motor), decodeNumber(decodeNumber_motor),
+      encoderPinA(encoder_PinA), encoderPinB(encoder_PinB),
+      setpoint(SetpointPID), input(InputPID), output(OutputPID),
+      Kp(KpPID), Ki(KiPID), Kd(KdPID),
+      current_error(0.0), limit_output_pid(limitOutputPID), sample_time_pid(sampleTimePID),
+      interrupt(false) {
+    // Constructor body if needed
+}
+
+Motor_Sneak::Motor_Sneak(int pinPWM, int pinINA, int pinINB,  
+                          const float PPR_motor, const float gearRatio_motor, const float decodeNumber_motor, 
+                          const int encoder_PinA, const int encoder_PinB, 
+                          double* InputPID, double* OutputPID, double* SetpointPID, 
+                          double KpPID, double KiPID, double KdPID, float limitOutputPID, float sampleTimePID,
+                          bool interruptFlag)
+    : pid(InputPID, OutputPID, SetpointPID, KpPID, KiPID, KdPID, DIRECT),  // Initialize PID object here
+      PWM(pinPWM), INA(pinINA), INB(pinINB),
+      PPR(PPR_motor), gearRatio(gearRatio_motor), decodeNumber(decodeNumber_motor),
+      encoderPinA(encoder_PinA), encoderPinB(encoder_PinB),
+      setpoint(SetpointPID), input(InputPID), output(OutputPID),
+      Kp(KpPID), Ki(KiPID), Kd(KdPID),
+      current_error(0.0), limit_output_pid(limitOutputPID), sample_time_pid(sampleTimePID),
+      interrupt(interruptFlag) {
+    // Constructor body if needed
+}
+
+void Motor_Sneak::setup_motor_driver(){
+  pinMode(PWM, OUTPUT);
+  pinMode(INA, OUTPUT);
+  pinMode(INB, OUTPUT);
+}
+
+void Motor_Sneak::setup_encoder(){
+  pinMode(encoderPinA, INPUT_PULLUP);
+  pinMode(encoderPinB, INPUT_PULLUP);
+}
+
+Motor_Sneak* Motor_Sneak::instance = nullptr;
+
+void Motor_Sneak::isrWrapper() {
+    // Call the handleEncoder() member function of the current instance
+    if (instance) {
+        instance->handleEncoder();
+    }
+}
+
+void Motor_Sneak::setup_interruption() {
+    // Set the static member instance to the current instance
+    instance = this;
+
+    // Attach the interrupt using isrWrapper as the ISR
+    attachInterrupt(digitalPinToInterrupt(encoderPinA), isrWrapper, CHANGE);
+}
+
+void Motor_Sneak::setup_pid(){
+  pid.SetMode(AUTOMATIC);
+  pid.SetOutputLimits(-limit_output_pid, limit_output_pid);  // Adjust output limits based on your motor driver
+  pid.SetSampleTime(sample_time_pid);  // Set PID sample time in milliseconds
+}
+
+void Motor_Sneak::setup_motor(){
+  
+}
+
+void Motor_Sneak::handleEncoder() {
+  // Read the current state of the two channels
+  int stateA = digitalRead(encoderPinA);
+  int stateB = digitalRead(encoderPinB);
+
+  // Update the encoder position based on the quadrature encoding
+  if (stateA == stateB) {
+    // Clockwise rotation
+    encoderPosition++;
+  } else {
+    // Counterclockwise rotation
+    encoderPosition--;
+  }
+}
---
+++ sneak_robot_motor/sneak_robot_motor.h
@@ -0,0 +1,64 @@
+#ifndef sneak_robot_motor_h
+#define sneak_robot_motor_h
+#define LIBRARY_VERSION	1.0.0
+
+#include <PID_v1.h>
+class Motor_Sneak
+{
+
+  public:
+  Motor_Sneak(int, int, int,  // Motor driver pins
+              const float, const float, const float, //Motor parameters
+              const int, const int, // Encoder pins
+              double*, double*, double*, double, double, double, float, float // PID parameters
+              );
+
+  Motor_Sneak(int, int, int,  // Motor driver pins
+              const float, const float, const float, //Motor parameters
+              const int, const int, // Encoder pins
+              double*, double*, double*, double, double, double, float, float, // PID parameters
+              bool); // Interrupt flag
+
+  //commonly used functions **************************************************************************
+  void setup_motor_driver();
+  void setup_encoder();
+  void setup_pid();
+  void setup_motor();
+  void setup_interruption();
+  
+
+
+  private:
+
+  static void isrWrapper(); // Static member function as a wrapper
+  static Motor_Sneak* instance; // Static member to hold the current instance pointer
+  void handleEncoder();
+
+
+  //################ Variables for snake robot #############
+  // ########## MOTOR DRIVER VARIABLES ##############
+  int PWM ;
+  int INA ;
+  int INB ;
+  // ########## MOTOR VARIABLES ##############
+  const float PPR ;
+  const float gearRatio ;
+  const float decodeNumber ;
+  double rotationalAngle = 0.0;
+  // ########## ENCODER VARIABLES ##############
+  const int encoderPinA ;  // Connect encoder channel A Motor 1 to digital pin 2
+  const int encoderPinB ;  // Connect encoder channel B Motor 1 to digital pin 2
+  volatile long encoderPosition = 0;
+  // ########## PID VARIABLES ##############
+  double *setpoint;
+  double *input, *output;
+  double Kp, Ki, Kd ;  // PID tuning parameters
+  PID pid;
+  //PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
+  float current_error = 0.0;
+  float limit_output_pid = 100;
+  float sample_time_pid = 10;
+  // ########## Interrupt VARIABLES ##############
+  bool interrupt = false;
+};
+#endif
